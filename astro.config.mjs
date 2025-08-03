9e02bc97ea95cf931bdf0b11529959905e9853d4
ailaosji <laosji.net@gmail.com>
2025-08-03 10:55:50 +0800

    feat: 更新 cards 页面和 config 配置，新增 news 页面

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
+  site: 'https://ailaosji.github.io',
+  base: isProd ? '/crydit/' : '/',
   integrations: [react(), tailwind()],
   markdown: {
     shikiConfig: {
